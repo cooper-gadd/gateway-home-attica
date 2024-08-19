459a161841d8d30598c1675cde0b8859c4a75962
cgadd21 <ctg7866@g.rit.edu>
2024-08-19 16:23:03 -0400

    supporters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,12 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+
 export default function Home() {
-  const platinumAngels = [
-    "Ralph Janes",
-    "Attica Package Co.",
-    "Jane A.C. Schmieder, Attorney",
-    "Attica Lyons Club",
-    "Wyoming Correctional Facility- Employee Benefit Fund",
-    "Darlene Collins",
-    "Bengo’s",
-  ];
-
-  const goldAngels = [
-    "Robinson Hackemer",
-    "Attica Real Estate Partnership",
-    "Advanced Dairy",
-    "Craddock Family Dental",
-    "Providence Fellowship",
-    "Charles & Carol Leah Klotzbach",
-  ];
-
-  const silverAngels = [
-    "L Toal Plumbing",
-    "Hardings Attica Furniture",
-    "Ron & Evelyn (Peach) Merrill",
-    "County Line Stone",
-    "Whiting Door",
-    "Western NY Concrete",
-    "Michael & Judith Trauscht",
-    "Connie Tartick",
-    "Jodi Rospierski",
-  ];
-
-  const bronzeAngels = [
-    "Lor-Rob Dairy Farm",
-    "Excel Fence",
-    "Joseph & Carol Pilc",
-    "Attica Rodeo Show & Association",
-    "Peter & Dawn Mark",
-    "Donald & Jane Beechler",
-    "Donna Ferry",
-    "Alden State Bank",
-    "Mary Rudolph",
-    "Willam Renz Jr.",
-    "Can-Can Redemption",
-    "Wendy McArthur",
-    "American Legion, Bliss",
-    "Trank Real Estate",
-    "Paul & Diane Luderman",
-  ];
-
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
@@ -64,49 +23,98 @@ export default function Home() {
         Funding is accomplished through donations, bequests, and community
         support. Admission is based solely on need.
       </p>
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-        Gateway Home Comfort Care’s 2022 Annual Campaign Angel Supporters
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-4">
-        <div>
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-gray-300">
-            Platinum Angel
-          </h3>
-          <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-            {platinumAngels.map((platinumAngel) => (
-              <li key={platinumAngel}>{platinumAngel}</li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-yellow-400">
-            Gold Angel
-          </h3>
-          <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-            {goldAngels.map((goldAngel) => (
-              <li key={goldAngel}>{goldAngel}</li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-gray-200">
-            Silver Angel
-          </h3>
-          <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-            {silverAngels.map((silverAngel) => (
-              <li key={silverAngel}>{silverAngel}</li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-amber-600">
-            Bronze Angel
-          </h3>
-          <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-            {bronzeAngels.map((bronzeAngel) => (
-              <li key={bronzeAngel}>{bronzeAngel}</li>
-            ))}
-          </ul>
+      <div>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          Supporters
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <Card>
+            <CardHeader className="pb-3">
+              <CardTitle className="text-gray-300">Platinum Angel</CardTitle>
+              <CardDescription className="max-w-lg text-balance leading-relaxed">
+                Gateway Home Comfort Care’s 2022 Annual Campaign Angel
+                Supporters
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <li>Ralph Janes</li>
+              <li>Attica Package Co.</li>
+              <li>Jane A.C. Schmieder, Attorney</li>
+              <li>Attica Lyons Club</li>
+              <li>Wyoming Correctional Facility- Employee Benefit Fund</li>
+              <li>Darlene Collins</li>
+              <li>Bengo’s</li>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="pb-3">
+              <CardTitle className="text-yellow-400">Gold Angel</CardTitle>
+              <CardDescription className="max-w-lg text-balance leading-relaxed">
+                Gateway Home Comfort Care’s 2022 Annual Campaign Angel
+                Supporters
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                <li>Robinson Hackemer</li>
+                <li>Attica Real Estate Partnership</li>
+                <li>Advanced Dairy</li>
+                <li>Craddock Family Dental</li>
+                <li>Providence Fellowship</li>
+                <li>Charles & Carol Leah Klotzbach</li>
+              </ul>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="pb-3">
+              <CardTitle className="text-gray-200">Silver Angel</CardTitle>
+              <CardDescription className="max-w-lg text-balance leading-relaxed">
+                Gateway Home Comfort Care’s 2022 Annual Campaign Angel
+                Supporters
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                <li>L Toal Plumbing</li>
+                <li>Hardings Attica Furniture</li>
+                <li>Ron & Evelyn (Peach) Merrill</li>
+                <li>County Line Stone</li>
+                <li>Whiting Door</li>
+                <li>Western NY Concrete</li>
+                <li>Michael & Judith Trauscht</li>
+                <li>Connie Tartick</li>
+                <li>Jodi Rospierski</li>
+              </ul>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="pb-3">
+              <CardTitle className="text-amber-600">Bronze Angel</CardTitle>
+              <CardDescription className="max-w-lg text-balance leading-relaxed">
+                Gateway Home Comfort Care’s 2022 Annual Campaign Angel
+                Supporters
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                <li>Lor-Rob Dairy Farm</li>
+                <li>Excel Fence</li>
+                <li>Joseph & Carol Pilc</li>
+                <li>Attica Rodeo Show & Association</li>
+                <li>Peter & Dawn Mark</li>
+                <li>Donald & Jane Beechler</li>
+                <li>Donna Ferry</li>
+                <li>Alden State Bank</li>
+                <li>Mary Rudolph</li>
+                <li>Willam Renz Jr.</li>
+                <li>Can-Can Redemption</li>
+                <li>Wendy McArthur</li>
+                <li>American Legion, Bliss</li>
+                <li>Trank Real Estate</li>
+                <li>Paul & Diane Luderman</li>
+              </ul>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
