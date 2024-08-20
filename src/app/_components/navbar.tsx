43b7d01ee9a9d1fac5a3df393789b9bef0036311
cgadd21 <ctg7866@g.rit.edu>
2024-08-20 12:59:43 -0400

    contact page

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,28 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
-import {
-  CircleUser,
-  Facebook,
-  Home,
-  Mail,
-  Map,
-  Menu,
-  Phone,
-  Printer,
-  Search,
-} from "lucide-react";
+import { Home, Menu, Search } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -45,6 +27,10 @@ export function Navbar() {
     {
       title: "Donate",
       href: "/donate",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
     },
   ];
 
@@ -117,54 +103,6 @@ export function Navbar() {
             />
           </div>
         </form>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="secondary" size="icon" className="rounded-full">
-              <CircleUser className="h-5 w-5" />
-              <span className="sr-only">Toggle contact us menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Contact Us</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <a
-              target="_blank"
-              href="https://www.google.com/maps/search/?api=1&query=91 Main Street, Attica, NY 14011"
-            >
-              <DropdownMenuItem className="flex items-center gap-4">
-                <Map className="h-5 w-5" />
-                91 Main Street, Attica, NY 14011
-              </DropdownMenuItem>
-            </a>
-            <a href="tel:585-708-4331">
-              <DropdownMenuItem className="flex items-center gap-4">
-                <Phone className="h-5 w-5" />
-                585-708-4331
-              </DropdownMenuItem>
-            </a>
-            <a href="fax:5857084333">
-              <DropdownMenuItem className="flex items-center gap-4">
-                <Printer className="h-5 w-5" />
-                585-708-4333
-              </DropdownMenuItem>
-            </a>
-            <a href="mailto:info@gatewayhomeattica.org">
-              <DropdownMenuItem className="flex items-center gap-4">
-                <Mail className="h-5 w-5" />
-                info@gatewayhomeattica.org
-              </DropdownMenuItem>
-            </a>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/gatewayhomeattica/"
-            >
-              <DropdownMenuItem className="flex items-center gap-4">
-                <Facebook className="h-5 w-5" />
-                Gateway Home
-              </DropdownMenuItem>
-            </a>
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
     </header>
   );
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,120 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+  CardContent,
+} from "@/components/ui/card";
+import { Mail, Map, Phone, Printer, ThumbsUp } from "lucide-react";
+import Link from "next/link";
+
+export default function Contact() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <Card>
+        <CardHeader className="pb-3">
+          <CardTitle>Contact Us</CardTitle>
+          <CardDescription className="max-w-lg text-balance leading-relaxed">
+            Open 24 hours
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          We are always here to help you. If you have any questions or need
+          help, feel free to contact us.
+        </CardContent>
+      </Card>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-5">
+        <Card>
+          <CardHeader className="pb-3">
+            <CardTitle>Address</CardTitle>
+            <CardDescription className="max-w-lg text-balance leading-relaxed">
+              91 Main Street, Attica, NY 14011
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild>
+              <Link
+                target="_blank"
+                href={
+                  "https://www.google.com/maps/search/?api=1&query=91 Main Street, Attica, NY 14011"
+                }
+              >
+                <Map className="mr-2 h-4 w-4" />
+                Find us
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader className="pb-3">
+            <CardTitle>Phone</CardTitle>
+            <CardDescription className="max-w-lg text-balance leading-relaxed">
+              585-708-4331
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild>
+              <Link href={"tel:585-708-4331"}>
+                <Phone className="mr-2 h-4 w-4" />
+                Call us
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader className="pb-3">
+            <CardTitle>Fax</CardTitle>
+            <CardDescription className="max-w-lg text-balance leading-relaxed">
+              585-708-4333
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild>
+              <Link href={"fax:5857084333"}>
+                <Printer className="mr-2 h-4 w-4" />
+                Fax us
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader className="pb-3">
+            <CardTitle>Email</CardTitle>
+            <CardDescription className="max-w-lg text-balance leading-relaxed">
+              info@gatewayhomeattica.org
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild>
+              <Link href={"mailto:info@gatewayhomeattica.org"}>
+                <Mail className="mr-2 h-4 w-4" />
+                Email us
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader className="pb-3">
+            <CardTitle>Facebook</CardTitle>
+            <CardDescription className="max-w-lg text-balance leading-relaxed">
+              Gateway Home
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild>
+              <Link
+                target="_blank"
+                href={"https://www.facebook.com/gatewayhomeattica/"}
+              >
+                <ThumbsUp className="mr-2 h-4 w-4" />
+                Like us
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
+}
