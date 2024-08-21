1846c0e1db8ee967079a732d6d01b18f522d0e53
cgadd21 <ctg7866@g.rit.edu>
2024-08-21 17:28:00 -0400

    instagram and removed policies

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ import {
   CardFooter,
   CardContent,
 } from "@/components/ui/card";
-import { Mail, Map, Phone, Printer, ThumbsUp } from "lucide-react";
+import { Mail, Map, Phone, Printer, ThumbsUp, UserPlus } from "lucide-react";
 import Link from "next/link";
 
 export default function Contact() {
@@ -25,7 +25,55 @@ export default function Contact() {
           help, feel free to contact us.
         </CardContent>
       </Card>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-5">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+        <Card>
+          <CardHeader className="pb-3">
+            <CardTitle>Phone</CardTitle>
+            <CardDescription className="max-w-lg text-balance leading-relaxed">
+              585-708-4331
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild>
+              <Link href={"tel:585-708-4331"}>
+                <Phone className="mr-2 h-4 w-4" />
+                Call us
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader className="pb-3">
+            <CardTitle>Email</CardTitle>
+            <CardDescription className="max-w-lg text-balance leading-relaxed">
+              info@gatewayhomeattica.org
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild>
+              <Link href={"mailto:info@gatewayhomeattica.org"}>
+                <Mail className="mr-2 h-4 w-4" />
+                Email us
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader className="pb-3">
+            <CardTitle>Fax</CardTitle>
+            <CardDescription className="max-w-lg text-balance leading-relaxed">
+              585-708-4333
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild>
+              <Link href={"fax:5857084333"}>
+                <Printer className="mr-2 h-4 w-4" />
+                Fax us
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
         <Card>
           <CardHeader className="pb-3">
             <CardTitle>Address</CardTitle>
@@ -49,54 +97,6 @@ export default function Contact() {
         </Card>
         <Card>
           <CardHeader className="pb-3">
-            <CardTitle>Phone</CardTitle>
-            <CardDescription className="max-w-lg text-balance leading-relaxed">
-              585-708-4331
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Button asChild>
-              <Link href={"tel:585-708-4331"}>
-                <Phone className="mr-2 h-4 w-4" />
-                Call us
-              </Link>
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader className="pb-3">
-            <CardTitle>Fax</CardTitle>
-            <CardDescription className="max-w-lg text-balance leading-relaxed">
-              585-708-4333
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Button asChild>
-              <Link href={"fax:5857084333"}>
-                <Printer className="mr-2 h-4 w-4" />
-                Fax us
-              </Link>
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader className="pb-3">
-            <CardTitle>Email</CardTitle>
-            <CardDescription className="max-w-lg text-balance leading-relaxed">
-              info@gatewayhomeattica.org
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Button asChild>
-              <Link href={"mailto:info@gatewayhomeattica.org"}>
-                <Mail className="mr-2 h-4 w-4" />
-                Email us
-              </Link>
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader className="pb-3">
             <CardTitle>Facebook</CardTitle>
             <CardDescription className="max-w-lg text-balance leading-relaxed">
               Gateway Home
@@ -110,6 +110,25 @@ export default function Contact() {
               >
                 <ThumbsUp className="mr-2 h-4 w-4" />
                 Like us
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader className="pb-3">
+            <CardTitle>Instagram</CardTitle>
+            <CardDescription className="max-w-lg text-balance leading-relaxed">
+              gatewayhomeattica
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild>
+              <Link
+                target="_blank"
+                href={"https://www.instagram.com/gatewayhomeattica/"}
+              >
+                <UserPlus className="mr-2 h-4 w-4" />
+                Follow us
               </Link>
             </Button>
           </CardFooter>
