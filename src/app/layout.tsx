0ed926035893a095241c3e6e2978bf0d9cc209a1
cgadd21 <ctg7866@g.rit.edu>
2024-08-22 16:33:50 -0400

    package updates and theme fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="">
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
