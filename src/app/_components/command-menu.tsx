1ffeaaf726b49db58cd59b5395ce9807fde4b444
cgadd21 <ctg7866@g.rit.edu>
2024-10-15 13:21:11 -0400

    removed command k in search

--- src/app/_components/command-menu.tsx
+++ src/app/_components/command-menu.tsx
@@ -70,9 +70,6 @@ export function CommandMenu({ ...props }: DialogProps) {
           <SearchIcon className="mr-2 h-4 w-4" />
           Search...
         </span>
-        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-          <span className="text-xs">⌘</span>K
-        </kbd>
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
         <CommandInput placeholder="Search..." />
