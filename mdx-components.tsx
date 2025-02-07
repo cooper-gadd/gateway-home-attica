dd3be291d25e38a7a00b802d321e1e5ce06ec303
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2025-02-06 20:06:47 -0500

    mdx styles

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,5 +3,59 @@ import type { MDXComponents } from "mdx/types";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    h1: ({ children }) => (
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        {children}
+      </h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        {children}
+      </h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
+        {children}
+      </h3>
+    ),
+    p: ({ children }) => (
+      <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>
+    ),
+    ul: ({ children }) => (
+      <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>
+    ),
+    blockquote: ({ children }) => (
+      <blockquote className="mt-6 border-l-2 pl-6 italic">
+        {children}
+      </blockquote>
+    ),
+    a: ({ children, href }) => (
+      <a
+        href={href}
+        className="font-medium text-primary underline underline-offset-4"
+      >
+        {children}
+      </a>
+    ),
+    table: ({ children }) => (
+      <div className="my-6 w-full overflow-y-auto">
+        <table className="w-full">{children}</table>
+      </div>
+    ),
+    thead: ({ children }) => <thead>{children}</thead>,
+    tbody: ({ children }) => <tbody>{children}</tbody>,
+    tr: ({ children }) => (
+      <tr className="m-0 border-t p-0 even:bg-muted">{children}</tr>
+    ),
+    th: ({ children }) => (
+      <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
+        {children}
+      </th>
+    ),
+    td: ({ children }) => (
+      <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+        {children}
+      </td>
+    ),
   };
 }
