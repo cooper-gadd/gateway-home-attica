2d80fdab47a0e94cfc234f51a540a01b00fa0a6b
Cooper Gadd <coopergadd@icloud.com>
2026-05-14 19:31:28 -0400

    rm matching

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -5,7 +5,6 @@ import {
   Car,
   Clock,
   Flag,
-  HandHeart,
   LandPlot,
   LucideProps,
   MapPin,
@@ -32,15 +31,6 @@ const events: {
   link?: string;
   linkText?: string;
 }[] = [
-  {
-    title: "Matching Campaign",
-    date: "April 1 - May 15, 2026",
-    description:
-      "A generous donor will match every gift up to $20,000 — but only through May 15th! Your donation doubles in impact, helping Gateway Home continue providing compassionate, no-cost end-of-life care.",
-    icon: HandHeart,
-    link: "https://gatewayhomeattica.networkforgood.com/projects/298367-matching-gifts-april-2026",
-    linkText: "Double My Gift",
-  },
   {
     title: "Letchworth Masonic Council Benefit Golf Tournament",
     date: "May 23, 2026",
